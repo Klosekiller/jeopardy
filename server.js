2d3463cad68a2e8345396b06e0f8ef8a5aaf2e78
Klosekiller <111622444+Klosekiller@users.noreply.github.com>
2024-08-03 00:38:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-// Set Pug as the template engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'src/views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const gameRoutes = require('./src/routes/gameRoutes');
 app.use('/api', gameRoutes);
 
+// Serve the compiled HTML files from the 'dist' directory
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Example additional route
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'about.html'));
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
